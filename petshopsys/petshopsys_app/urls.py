483c889a1e0f217e893ec4ffb1638dce35349f12
larissabreu <49702165+larissabreu@users.noreply.github.com>
2019-11-27 15:44:24 -0400

    cart add

--- petshopsys/petshopsys_app/urls.py
+++ petshopsys/petshopsys_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name = 'login'),
+    path('login/', views.login, name = 'login'),
+    path('',views.index, name = 'index'),
     path('menu_cliente/',views.menu_cliente,name='menu_cliente'),
     path('menu_gerente/',views.menu_gerente,name='menu_gerente'),
     path('menu_caixa/',views.menu_caixa,name='menu_caixa'),
--- petshopsys/petshopsys_app/views.py
+++ petshopsys/petshopsys_app/views.py
@@ -10,11 +10,14 @@ from .models import Usuario
 def login(request):
     return render(request,'petshopsys_app/login.html')
 
+def index(request):
+    return render(request,'petshopsys_app/index.html')
+    
 def menu_cliente(request):
     return HttpResponse(request,'petshopsys_app/informações-pet.html')
 
 def menu_gerente(HttpResponse):
-    return HttpResponse("Aqui vem o menu do genrente")
+    return HttpResponse("Aqui vem o menu do gerente")
 
 def menu_caixa(request):
     return HttpResponse("Aqui vem o menu do caixa")
