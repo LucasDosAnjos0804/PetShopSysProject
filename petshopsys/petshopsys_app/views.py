42f426c4fb48fdbb040676e4e42e97d494302d76
LucasDosAnjos0804 <anjoslucas7@gmail.com>
2019-11-27 11:35:06 -0400

    aff

--- petshopsys/petshopsys_app/views.py
+++ petshopsys/petshopsys_app/views.py
@@ -8,14 +8,18 @@ from .models import Usuario
 
 
 def login(request):
-
     return render(request,'petshopsys_app/login.html')
+
 def menu_cliente(request):
     return HttpResponse("Aqui vem o menu do cliente")
+
 def menu_gerente(HttpResponse):
     return HttpResponse("Aqui vem o menu do genrente")
+
 def menu_caixa(request):
     return HttpResponse("Aqui vem o menu do caixa")
+
 def menu_veterinario(request):
     return HttpResponse("Aqui vem o menu do veterinario")
+    
 
