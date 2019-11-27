f3fee65b5c23a13ea1f64b7af06851b47b77def0
LucasDosAnjos0804 <anjoslucas7@gmail.com>
2019-11-27 14:34:42 -0400

    vai

---
+++ petshopsys/petshopsys_app/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Cliente,Gerente,Caixa,Veterinario,Pet,RegistrarConsulta,Servico,Fornecedor,Produto,Estoque,ItensCompra,ItensServico,Compra,NotaFiscal
+
+
+
+admin.site.register(Cliente)
+admin.site.register(Gerente)
+admin.site.register(Caixa)
+admin.site.register(Veterinario)
+admin.site.register(Pet)
+admin.site.register(RegistrarConsulta)
+admin.site.register(Servico)
+admin.site.register(Fornecedor)
+admin.site.register(Produto)
+admin.site.register(Estoque)
+admin.site.register(ItensCompra)
+admin.site.register(ItensServico)
+admin.site.register(Compra)
+admin.site.register(NotaFiscal)
