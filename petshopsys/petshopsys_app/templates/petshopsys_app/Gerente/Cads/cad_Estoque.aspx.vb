93507fc1cff8b70041a83d75e2c430353541352d
larissabreu <49702165+larissabreu@users.noreply.github.com>
2019-11-30 14:16:19 -0400

    aspx in trash

--- petshopsys/petshopsys_app/templates/petshopsys_app/Gerente/Cads/cad_Estoque.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class cad_Estoque
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-    End Sub
-
-End Class
