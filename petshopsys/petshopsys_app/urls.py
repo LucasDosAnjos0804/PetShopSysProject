e0c752694678107e73a9f3c3e3378f0062c36467
LucasDosAnjos0804 <anjoslucas7@gmail.com>
2019-12-04 00:20:19 -0400

    FIX urls personalizadas do gerente

--- petshopsys/petshopsys_app/urls.py
+++ petshopsys/petshopsys_app/urls.py
@@ -12,60 +12,61 @@ urlpatterns = [
     path('gerente/',MenuGerente.as_view(),name='MenuGerente'),
     path('caixa/',MenuCaixa.as_view(),name='MenuCaixa'),
     path('veterinario/',MenuVeterinario.as_view(),name='MenuVeterinario'),
-
-    path('cadFornecedor/',views.cadFornecedor,name='CadFornecedor'),
-    path('listFornecedor/',views.listFornecedor,name='ListFornecedor'),
-    path('editFornecedor/<int:pk>',views.editFornecedor,name='EditFornecedor'),
-    path('delFornecedor/<int:pk>',views.delFornecedor,name='DelFornecedor'),
-
-
-    path('cadServico/',views.cadServico,name='CadServico'),
-    path('listServico/',views.listServico,name='ListServico'),
-    path('editServico/<int:pk>',views.editServico,name='EditServico'),
-    path('delServico/<int:pk>',views.delServico,name='DelServico'),
+#####################GERENTE#######################
+    path('gerente/cadFornecedor/',views.cadFornecedor,name='CadFornecedor'),
+    path('gerente/listFornecedor/',views.listFornecedor,name='ListFornecedor'),
+    path('gerente/editFornecedor/<int:pk>',views.editFornecedor,name='EditFornecedor'),
+    path('gerente/delFornecedor/<int:pk>',views.delFornecedor,name='DelFornecedor'),
 
 
-    path('cadCliente/',views.cadCliente,name='CadCliente'),
-    path('listCliente/',views.listCliente,name='ListCliente'),
-    path('editCliente/<int:pk>',views.editCliente,name='EditCliente'),
-    path('delCliente/<int:pk>',views.delCliente,name='DelCliente'),
+    path('gerente/cadServico/',views.cadServico,name='CadServico'),
+    path('gerente/listServico/',views.listServico,name='ListServico'),
+    path('gerente/editServico/<int:pk>',views.editServico,name='EditServico'),
+    path('gerente/delServico/<int:pk>',views.delServico,name='DelServico'),
 
 
-    path('cadPet/',views.cadPet,name='CadPet'),
-    path('listPet/',views.listPet,name='ListPet'),
-    path('editPet/<int:pk>',views.editPet,name='EditPet'),
-    path('delPet/<int:pk>',views.delPet,name='DelPet'),
+    path('gerente/cadCliente/',views.cadCliente,name='CadCliente'),
+    path('gerente/listCliente/',views.listCliente,name='ListCliente'),
+    path('gerente/editCliente/<int:pk>',views.editCliente,name='EditCliente'),
+    path('gerente/delCliente/<int:pk>',views.delCliente,name='DelCliente'),
 
 
-    path('cadProduto/',views.cadProduto,name='CadProduto'),
-    path('listProduto/',views.listProduto,name='ListProduto'),
-    path('editProduto/<int:pk>',views.editProduto,name='EditProduto'),
-    path('delProduto/<int:pk>',views.delProduto,name='DelProduto'),
+    path('gerente/cadPet/',views.cadPet,name='CadPet'),
+    path('gerente/listPet/',views.listPet,name='ListPet'),
+    path('gerente/editPet/<int:pk>',views.editPet,name='EditPet'),
+    path('gerente/delPet/<int:pk>',views.delPet,name='DelPet'),
+
+
+    path('gerente/cadProduto/',views.cadProduto,name='CadProduto'),
+    path('gerente/listProduto/',views.listProduto,name='ListProduto'),
+    path('gerente/editProduto/<int:pk>',views.editProduto,name='EditProduto'),
+    path('gerente/delProduto/<int:pk>',views.delProduto,name='DelProduto'),
 
 
     path('CadFuncionario/',views.cadFuncionario,name='CadFuncionario'),
 
-    path('cadGerente/',views.cadGerente,name='CadGerente'),
-    path('listGerente/',views.listGerente,name='ListGerente'),
-    path('editGerente/<int:pk>',views.editGerente,name='EditGerente'),
-    path('delGerente/<int:pk>',views.delGerente,name='DelGerente'),
+    path('CadFuncionario/gerente/cadGerente/',views.cadGerente,name='CadGerente'),
+    path('CadFuncionario/gerente/listGerente/',views.listGerente,name='ListGerente'),
+    path('CadFuncionario/gerente/editGerente/<int:pk>',views.editGerente,name='EditGerente'),
+    path('CadFuncionario/gerente/delGerente/<int:pk>',views.delGerente,name='DelGerente'),
 
 
-    path('cadVeterinario/',views.cadVeterinario,name='CadVeterinario'),
-    path('listVeterinario/',views.listVeterinario,name='ListVeterinario'),
-    path('editVeterinario/<int:pk>',views.editVeterinario,name='EditVeterinario'),
-    path('delVeterinario/<int:pk>',views.delVeterinario,name='DelVeterinario'),
+    path('CadFuncionario/gerente/cadVeterinario/',views.cadVeterinario,name='CadVeterinario'),
+    path('CadFuncionario/gerente/listVeterinario/',views.listVeterinario,name='ListVeterinario'),
+    path('CadFuncionario/gerente/editVeterinario/<int:pk>',views.editVeterinario,name='EditVeterinario'),
+    path('CadFuncionario/gerente/delVeterinario/<int:pk>',views.delVeterinario,name='DelVeterinario'),
 
 
-    path('cadCaixa/',views.cadCaixa,name='CadCaixa'),
-    path('listCaixa/',views.listCaixa,name='ListCaixa'),
-    path('editCaixa/<int:pk>',views.editCaixa,name='EditCaixa'),
-    path('delCaixa/<int:pk>',views.delCaixa,name='DelCaixa'),
+    path('CadFuncionario/gerente/cadCaixa/',views.cadCaixa,name='CadCaixa'),
+    path('CadFuncionario/gerente/listCaixa/',views.listCaixa,name='ListCaixa'),
+    path('CadFuncionario/gerente/editCaixa/<int:pk>',views.editCaixa,name='EditCaixa'),
+    path('CadFuncionario/gerente/delCaixa/<int:pk>',views.delCaixa,name='DelCaixa'),
 
 
-    path('cadEstoque/',views.cadEstoque,name='CadEstoque'),
-    path('listEstoque/',views.listEstoque,name='ListEstoque'),
-    path('editEstoque/<int:pk>',views.editEstoque,name='EditEstoque'),
-    path('delEstoque/<int:pk>',views.delEstoque,name='DelEstoque'),
+    path('gerente/cadEstoque/',views.cadEstoque,name='CadEstoque'),
+    path('gerente/listEstoque/',views.listEstoque,name='ListEstoque'),
+    path('gerente/editEstoque/<int:pk>',views.editEstoque,name='EditEstoque'),
+    path('gerente/delEstoque/<int:pk>',views.delEstoque,name='DelEstoque'),
+########################################################
 
 ]
